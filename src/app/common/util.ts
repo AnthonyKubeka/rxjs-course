31bb9c3d94a64f17fc360652a36bb89515dd412b
= <nmkubeka@gmail.com>
2023-05-27 21:59:21 +0200

    chapter 2.13 commit

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,17 @@
+import { Observable } from "rxjs";
 
+export function createHttpObservable(route: string): Observable<any>{
+  const http$ = new Observable(observer => {
+    fetch(route).then(res => {
+      return res.json();
+    }).then((body) => {
+      observer.next(body);
+      observer.complete();
+    }).catch( error => {
+      observer.error(error);
+    })
+  });
+
+  return http$;
+}
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -11,15 +12,30 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
     }
 
-    ngOnInit() {
+  ngOnInit() {
 
+    const http$ : Observable<Course[]> = createHttpObservable('/api/courses'); // extracted into seperate module
 
+    const courses$ : Observable<Course[]> = http$.pipe(
+      shareReplay(),
+      map(response => Object.values(response["payload"])),
+      );
 
-    }
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category == 'BEGINNER'))
+    );
+
+    this.advancedCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category == 'BEGINNER'))
+    );
+
+  }
 
 }
